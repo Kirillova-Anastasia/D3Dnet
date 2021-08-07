62f218748996289799bcae9de776ae2d4991e955
ansty <kirillovaap@yandex.ru>
2021-08-07 23:15:22 +0300

    script

--- D3Dnet.sh
+++ D3Dnet.sh
@@ -2,3 +2,5 @@ pip install torch==1.0.0 torchvision==0.2.2 pillow==6.2.1 matplotlib
 cd code/dcn
 bash make.sh
 cd ..
+
+python3 eval.py --test_dataset_dir /dataset --dataset test2_gauss_noise2 --result_dir /output/D3Dnet
